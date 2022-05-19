e4db9881ab88ad94ba7e06f6d53b62aa57bb80c6
suan hwee <suanhwee@gmail.com>
2022-05-19 14:41:46 +0800

    tut4

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Typography } from '@material-ui/core'
 import { Button } from '@material-ui/core'
 import { Container } from '@material-ui/core'
+import AcUnitIcon from '@material-ui/icons/AcUnit';
+import { AcUnitOutlined } from '@material-ui/icons';
+import SendIcon from '@material-ui/icons/Send';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 export default function Create() {
   return (
@@ -19,9 +23,20 @@ export default function Create() {
     onClick={()=>console.log("you clicked me")}
     type="submit" 
     color="secondary" 
-    variant="contained" >
+    variant="contained" 
+    
+    endIcon={<KeyboardArrowRightIcon />}
+    >
+
       Submit
     </Button>
+    {/* <br />
+    <AcUnitOutlined />
+    <AcUnitOutlined color="secondary" fontSize='large' />
+    <AcUnitOutlined color="secondary" fontSize='small' />
+    <AcUnitOutlined color="action" fontSize='small' />
+    <AcUnitOutlined color="error" fontSize='small' />
+    <AcUnitOutlined color="disabled" fontSize='small' /> */}
 
     {/* <Button type="submit">SUBMIT</Button>
     <Button type="submit" color="secondary" variant="outlined">SUBMIT</Button>
